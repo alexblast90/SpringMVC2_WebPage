1a7fef85bc5ed76eef444fc1bf897658ce74fde0
alexblast90 <[alexblast90@gmail.com]>
2023-06-08 17:54:10 +0900

    김영한 SpringMVC2 웹페이지 제작(Item-Service)

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,25 @@
+package hello.itemservice.domain.item;
+import lombok.Data;
+
+
+@Data
+public class Item {
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    //기본생성자
+    public Item() {
+
+    }
+
+
+    //Id를 제외한 생성자
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+}
